4f229c0947bb5f2eea9fa6d4bc2f773768e6e637
Aedi <popibobina23_@outlook.es>
2025-07-20 16:43:04 -0700

    Primer Commit

---
+++ For/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numero = new int[5];
+        int suma;
+        int i;
+
+        for (i = 0; i <= 4; i++)
+        {
+            Console.WriteLine("Introduce el dato numero {0}: ", i + 1);
+            numero[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        suma = 0;
+        for (i = 0; i <= 4; i++)
+            suma += numero[i];
+
+        Console.WriteLine("Su suma es: {0}", suma);
+    }
+}
---
+++ Python.py
@@ -0,0 +1,14 @@
+a = float(input("Asigna primer numero:" ))
+s = input("Asigna la operacion (+,-,*,/):" )
+b = float(input("Asigna el segundo numero:" ))
+
+if s == "*":10
+    print(a * b)
+elif s == "+":
+    print(a + b)
+elif s == "-":
+    print(a - b)
+elif s == "/":
+    print(a / b)
+else:
+    print("Error al elegir la operación")
